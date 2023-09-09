1e49677812c79f15af09ba4c36ba8315a51da1bb
NicolasOviedoRubiano <nicolas.or97@gmail.com>
2023-09-09 08:43:52 -0500

    order en payment success incluye los productos

--- src/Controllers/Order/paymentSuccessController.js
+++ src/Controllers/Order/paymentSuccessController.js
@@ -1,7 +1,12 @@
 const { Order, Products } = require("../../db");
 
 const paymentSuccessController = async (id_user) => {
-  const order = await Order.findOne({ where: { id_user, status: "cart" } });
+  const order = await Order.findOne({
+    where: { id_user, status: "cart" },
+    include: [
+      { model: Products, attributes: ["id_producto", "nombre", "precio"] },
+    ],
+  });
   order.status = "in-process";
   await order.save();
   await Order.create({ id_user, status: "cart" });
