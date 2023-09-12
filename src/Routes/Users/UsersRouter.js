af6d2dfc5ecdaadf4064bc32131e8ab3726df155
Gutierreznp <gutierreznp@gmail.com>
2023-09-12 11:08:11 -0300

    Se agregaron controllers para users

--- src/Routes/Users/UsersRouter.js
+++ src/Routes/Users/UsersRouter.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const deleteUser = require("../../Controllers/Users/deleteUser");
 const usersRouter = Router();
 const getAllUsers = require("../../Controllers/Users/getAllUsers");
 const getUserById = require("../../Controllers/Users/getUserById");
@@ -6,10 +7,15 @@ const getUserByUsername = require("../../Controllers/Users/getUserByUsername");
 const getUserByEmailAndPassword = require("../../Controllers/Users/getUserByEmailAndPassword");
 const createUser = require("../../Controllers/Users/createUser");
 const updateUser = require("../../Controllers/Users/updateUser");
-const deleteUser = require("../../Controllers/Users/deleteUser");
 const verifyToken = require("../../Assessments/verifyToken");
 const verifyAdmin = require("../../Assessments/verifyAdmin");
 const loginHandler = require("../../Handlers/User/loginHandler");
+const getEliminatedUsers = require("../../Controllers/Users/getEliminatedUsers");
+const restoreUser = require("../../Controllers/Users/restoreUser");
+
+usersRouter.get("/eliminados", getEliminatedUsers);
+
+usersRouter.post("/:id", restoreUser);
 
 usersRouter.post("/login", loginHandler); // ? OK
 
