59beaffbe471b6a5c73a6044ebdd6422767b4cab
NicolasOviedoRubiano <nicolas.or97@gmail.com>
2023-09-18 12:02:52 -0500

    Complemento de manejo de stock y precio

--- src/Controllers/Order/deleteProductController.js
+++ src/Controllers/Order/deleteProductController.js
@@ -2,7 +2,9 @@ const { Order, OrderProduct, Products } = require("../../db");
 const getCartForUser = require("./getCartForUser.js");
 
 const deleteProductController = async (id_producto, id_user) => {
-  const order = await Order.findOne({ where: { id_user, status: "cart" } });
+  const order = await Order.findOne({
+    where: { id_user, status: "cart" },
+  });
   const id_order = order.id_order;
   const orderProduct = await OrderProduct.findOne({
     where: { OrderIdOrder: id_order, ProductIdProducto: id_producto },
@@ -14,8 +16,22 @@ const deleteProductController = async (id_producto, id_user) => {
   product.stock += quantityInOrder;
   await product.save();
 
- await orderProduct.destroy();
- 
+  await orderProduct.destroy();
+
+  const orderUpdated = await Order.findByPk(id_order, {
+    include: [
+      {
+        model: Products,
+        attributes: ["precio"],
+      },
+    ],
+  });
+  orderUpdated.price = orderUpdated.Products.reduce(
+    (acc, product) => acc + product.OrderProduct.price,
+    0
+  );
+  await orderUpdated.save();
+
   const userCart = await getCartForUser(id_user);
   return userCart;
 };
