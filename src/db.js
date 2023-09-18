252a43c28a758115f4439409cef20f91effaa79b
NicolasOviedoRubiano <nicolas.or97@gmail.com>
2023-09-18 16:23:59 -0500

    Conexión a la base de datos del deploy

--- src/db.js
+++ src/db.js
@@ -20,10 +20,10 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 ? path __filename: la variable basename contendrá el nombre del archivo sin la ruta del directorio, es decir, solo el nombre del archivo en sí. Esto puede ser útil en escenarios donde necesitas referenciar el nombre del archivo actual sin la ruta completa*/
 
 //*mySQL
-const sequelize = new Sequelize(
-  `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  { logging: false, native: false, host: "localhost", dialect: "mysql" }
-);
+// const sequelize = new Sequelize(
+//   `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//   { logging: false, native: false, host: "localhost", dialect: "mysql" }
+// );
 //*Postgress
 // const sequelize = new Sequelize(
 //   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
@@ -34,10 +34,10 @@ const sequelize = new Sequelize(
 // );
 
 //*Postgress Deploy
-// const sequelize = new Sequelize(DB_DEPLOY, {
-//   logging: false,
-//   native: false,
-// });
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false,
+  native: false,
+});
 const basename = path.basename(__filename);
 
 /*
